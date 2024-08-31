10ec8ee3bb6e9c5e8df17a5beb8411afc6d087c5
rajshashank <rajshashank917@gmail.com>
2024-08-31 13:46:45 +0530

    revision

--- cruise.py
+++ cruise.py
@@ -1,20 +1,16 @@
-# 4) A party has been organised on cruise. The party is organised for a limited time(T). The
-# number of guests entering (E[i]) and leaving (L[i]) the party at every hour is represented as
-# elements of the array. The task is to find the maximum number of guests present on the cruise
+# 4) A party has been organised on cruise. 
+# The party is organised for a limited time(T). 
+# The number of guests entering (E[i]) and leaving (L[i]) 
+# The task is to find the maximum number of guests present on the cruise
 # at any given instance within T hours.
 
 # Example 1:
 # Input :
 
-# 5 -&gt; Value of T
-# [7,0,5,1,3] -&gt; E[], Element of E[0] to E[N-1], where input each element is separated by new
-# line
-# [1,2,1,3,4] -&gt; L[], Element of L[0] to L[N-1], while input each element is separate by new
-# line.
-# Output :
-# 8 -&gt; Maximum number of guests on cruise at an instance.
-
-
+# 5 Value of T
+# [7,0,5,1,3] E[], Element of E[0] to E[N-1]
+# [1,2,1,3,4] L[], Element of L[0] to L[N-1] 
+# Output : 8 Maximum number of guests on cruise at an instance.
 
 def max_guests(T, E, L):
     current_guests = 0

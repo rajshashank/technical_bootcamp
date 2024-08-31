8752211783c189db32c95234c7ad8c720eb06600
rajshashank <rajshashank917@gmail.com>
2024-08-31 13:54:13 +0530

    revision

--- cruise.py
+++ cruise.py
@@ -12,22 +12,22 @@
 # [1,2,1,3,4] L[], Element of L[0] to L[N-1] 
 # Output : 8 Maximum number of guests on cruise at an instance.
 
-def max_guests(T, E, L):
+def max_guests(t, e, l):
     current_guests = 0
     max_guests = 0
 
-    for i in range(T):
-        current_guests += E[i] - L[i]
+    for i in range(t):
+        current_guests += e[i] - l[i]
         if current_guests > max_guests:
             max_guests = current_guests
 
     return max_guests
 
-T = 5
-E = [7, 0, 5, 1, 3]
-L = [1, 2, 1, 3, 4]
-if T <= len(E) :
-    result = max_guests(T, E, L)
+t = 1
+e = [7, 0, 5, 1, 3]
+l = [1, 2, 1, 3, 4]
+if t <= len(e) :
+    result = max_guests(t, e, l)
 else:
     result = "Out of hours"
 print(result)  
